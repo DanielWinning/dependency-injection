1fdbab237cbe3d73a612684d6928fd5fe75e3021
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-09-06 21:05:38 +0100

    add unit tests for dependency container

---
+++ tests/DependencyContainerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace DannyXCII\DependencyInjectionComponentTests;
+
+use DannyXCII\DependencyInjectionComponent\DependencyContainer;
+use DannyXCII\DependencyInjectionComponent\Exception\NotFoundException;
+use PHPUnit\Framework\TestCase;
+
+class DependencyContainerTest extends TestCase
+{
+    /**
+     * @return void
+     *
+     * @throws NotFoundException
+     */
+    public function testAddAndGet(): void
+    {
+        $container = new DependencyContainer();
+
+        $container->add('app.lang', 'en_gb');
+
+        $this->assertEquals('en_gb', $container->get('app.lang'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHas(): void
+    {
+        $container = new DependencyContainer();
+
+        $container->add('app.lang', 'en_gb');
+
+        $this->assertTrue($container->has('app.lang'));
+        $this->assertFalse($container->has('el'));
+    }
+
+    /**
+     * @return void
+     *
+     * @throws NotFoundException
+     */
+    public function testGetNonExistentKeyThrowsNotFoundException(): void
+    {
+        $container = new DependencyContainer();
+
+        $this->expectException(NotFoundException::class);
+        $container->get('app');
+    }
+
+    /**
+     * @return void
+     *
+     * @throws NotFoundException
+     */
+    public function testResolveService(): void
+    {
+        $container = new DependencyContainer();
+
+        $service = new \stdClass();
+        $service->testString = 'Test Service';
+
+        $container->add('test', $service);
+        $resolvedService = $container->get('test');
+
+        $this->assertSame($service, $resolvedService);
+        $this->assertEquals($service->testString, $resolvedService->testString);
+    }
+}
